13e2cad1934ff9bcaed1f4998424e6e767022bd9
lukerpig <lukerpig@gmail.com>
2012-06-28 16:40:23 -0400

    Further SVG development

--- svg2.php
+++ svg2.php
@@ -8,10 +8,10 @@
 	<script src="http://d3js.org/d3.v2.js"></script>
 	<script type="text/javascript">
 	
-		svgW=1200;
+		svgW=1200, ratH=9, ratW=16;
 		var svgOpt = {
 			w: svgW,
-			h: svgW*9/16
+			h: svgW*ratH/ratW
 		}
 		var boxOpt = {
 			w: 16,
@@ -26,37 +26,67 @@
 		
 		var data = [
 		{
-			a: 16,
+			a: 1,
 			h: getHeight,
 			w: getWidth
 		},
 		{
-			a: 32,
+			a: 20,
 			h: getHeight,
 			w: getWidth
 		},
 		{
-			a: 64,
+			a: 300,
 			h: getHeight,
 			w: getWidth
 		},
 		{
-			a: 128,
+			a: 4000,
 			h: getHeight,
 			w: getWidth
 		},
 		{
-			a: 256,
+			a: 90000,
 			h: getHeight,
 			w: getWidth
 		},
 		{
-			a: 512,
+			a: 900000,
 			h: getHeight,
 			w: getWidth
 		},
 		{
-			a: 2024,
+			a: 1000000,
+			h: getHeight,
+			w: getWidth
+		},
+		{
+			a: 10000000,
+			h: getHeight,
+			w: getWidth
+		},
+		{
+			a: 100000000,
+			h: getHeight,
+			w: getWidth
+		},
+		{
+			a: 1000000000,
+			h: getHeight,
+			w: getWidth
+		},
+		{
+			a: 10000000000,
+			h: getHeight,
+			w: getWidth
+		},
+		{
+			a: 100000000000,
+			h: getHeight,
+			w: getWidth
+		},
+		{
+			a: 1000000000000,
 			h: getHeight,
 			w: getWidth
 		}
@@ -65,9 +95,16 @@
 		function onLoad(){
 			var svg = d3.select("body").append("svg")
 							.attr("height", svgOpt.h+"px")
-							.attr("width", svgOpt.w+"px")
-		var g= svg.append("g").attr("class","zoomer")
-					//.attr("transform","translate(0,-200) scale(2.5)")
+							.attr("viewBox","0 0 "+svgOpt.w+" "+svgOpt.h+"")
+							.attr("preserveAspectRatio","xMinYMax")
+							.attr("id","svg-box")
+							.style("width","100%")
+							.style("max-width","1200px")
+							.style("border","1px solid red");
+			var svgElem = document.getElementById("svg-box");
+			var svgH = svgElem.offsetHeight; var svgW = svgElem.offsetWidth;
+			svgHeight();
+			var g= svg.append("g").attr("class","zoomer")
 					
 			var numbers = g.selectAll("rect").data(data);
 			numbers.enter().append("rect")
@@ -75,28 +112,40 @@
 						function(d,i){
 							var w = 0;
 							for (var j = 0; j < i; j++) {
-								var margin = (i < data.length) ? data[j+1].w()*.02 : 0;
+								var margin = (i < data.length) ? data[j+1].w()*.005 : 0;
 								w += data[j].w()+margin;
 							} 
 							return w;
 						}
 					)
-				.attr("y",function(d,i){return svgOpt.h-50-d.h()})
+				.attr("y","0")
 				.attr("height", function(d,i){return d.h()})
 				.attr("width", function(d,i){return d.w()});
 				
+			//Size SVG
 			sizeSvg();
 			function sizeSvg() {
-				//12300/1.03, 200: 1.01, 66.7
-				//1300/	1.03, 21: 1.01, 7
-				//1: 0, 2: 312.5, 4: 468.75, 8: 546.875
-				var scale = 16;
-				var ratio = 1/scale;
-				console.log(ratio + "ratio");
-				g.attr("transform","scale("+scale+") translate(0,"+- 585.9375+")");
-				console.log(svgOpt.h);
+				var size = 99000000;
+				var scale = 1/size;
+				g.attr("transform","translate(0,"+(-50+svgOpt.h)+") scale("+scale+","+-scale+")");
 			}
 			
+			//WINDOW RESIZE
+			window.addEventListener('resize', windowResize, false);
+			var ww = window.innerWidth;
+			
+			function windowResize(){
+				var ww2=window.innerWidth;
+				if(ww2!=ww){ww = ww2;	svgHeight();}
+			}
+			
+			function svgHeight(){
+				svgW = svgElem.offsetWidth;
+				svgH = svgElem.offsetWidth*ratH/ratW;
+				svg.style("height",svgH+"px");
+			}
+			
+			//SLIDER
 			var slider = new Dragdealer('my-slider',
 				{
 					steps: 30,
@@ -110,14 +159,13 @@
 		}
 	</script>
 </head>
+<!--<svg>
+<g transform="translate(0,200), scale(13.13,-13.13)">
+<rect x="0" y="0" width="2" height="1" />
+</g>
+</svg>​-->
 
 <body onload="onLoad()">
-
-
-
-
-
-
 
 <div id="my-slider" class="dragdealer">
 	<div class="red-bar handle">drag me</div>
