ab055e3717f3cb393f78d03a197fabb07bbd3aac
lukerpig <lukerpig@gmail.com>
2012-07-11 21:44:19 -0400

    videoPause working, added watercost

---
+++ site/contentData.js
@@ -0,0 +1,37 @@
+var contentData = [
+	{
+		id : "story0",
+		url: "story/story0.html",
+		level: 2,
+		cuePoint: 0.000,
+	},
+	{
+		id : "watercost",
+		url: "story/watercost.html",
+		level: 2,
+		cuePoint: 5.000,
+	},
+	
+	{
+		id : "caption0",
+		captionTitle: "This is level one",
+		captionContent: "Content goes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. ",
+		level: 1,
+		cuePoint: 10.000,
+	},
+	{
+		id : "caption1",
+		captionTitle: "This is level one also",
+		captionContent: "Content goes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. Content ggoes herey. ",
+		level: 1,
+		cuePoint: 15.000,
+	},
+
+	
+	{
+		id : "story2",
+		url: "story/story2.html",
+		level: 3,
+		cuePoint: 20.000,
+	},
+];
