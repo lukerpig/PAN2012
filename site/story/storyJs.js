22e033ea13e685aa1c96dffd0f8eeb3c0577fcc8
lukerpig <lukerpig@gmail.com>
2012-07-12 22:21:14 -0400

    New Stories

---
+++ site/story/storyJs.js
@@ -0,0 +1,9 @@
+var video;
+function vidSize(vid){
+	video = vid;
+	var vidW = vid.offsetWidth;
+	var vidH = vid.offsetWidth*9/16;
+	vid.style.height=vidH+"px";
+}
+
+window.addEventListener('resize', function(){ vidSize(video) }, false);
