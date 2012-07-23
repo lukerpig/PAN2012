3970f6212765da38c4dd69693532e6c1f4cc6097
lukerpig <lukerpig@gmail.com>
2012-07-23 19:10:53 -0400

    Color work, x on captions

--- site/story/storyJs.js
+++ site/story/storyJs.js
@@ -1,7 +1,7 @@
 var video;
 function vidSize(vid){
 	video = vid;
-	var vidW = vid.offsetWidth*.7;
+	var vidW = vid.offsetWidth;
 	var vidH = vidW*9/16;
 	vid.style.width=vidW+"px";
 	vid.style.height=vidH+"px";
