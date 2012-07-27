194ab8bbb57e876cc7b519736f0c837edf4f0f3d
lukerpig <lukerpig@gmail.com>
2012-07-27 14:12:13 -0400

    mosaic work, about and preview

--- site/story/storyJs.js
+++ site/story/storyJs.js
@@ -4,6 +4,7 @@ function vidSize(vid){
 	var vidW = vid.offsetWidth;
 	var vidH = vidW*9/16;
 	vid.style.height=vidH+"px";
+	console.log(vidW+" "+vidH)
 }
 
 window.addEventListener('resize', function(){ if(video) vidSize(video) }, false);
