cd4874952ef84a97737cc114f55f4c331102957e
lukerpig <lukerpig@gmail.com>
2012-07-27 16:00:26 -0400

    fixed watercost

--- site/story/storyJs.js
+++ site/story/storyJs.js
@@ -2,7 +2,5 @@ function vidSize(vid){
 	var vidW = vid.offsetWidth;
 	var vidH = vidW*9/16;
 	vid.style.height=vidH+"px";
-	console.log(vidW+" "+vidH+" "+vid.id)
-	
-	window.addEventListener('resize', function(){ if(vid) vidSize(vid) }, false);
+	window.addEventListener('resize', function(){ vidSize(vid) }, false);
 }
